70eed6a4bb05a655eb920073b985c60c28a6d647
Tommaso Moretti <tommasomoretti88@gmail.com>
2023-11-30 17:08:57 +0100

    Update anal.js

--- anal.js
+++ anal.js
@@ -3,7 +3,7 @@
 function sendData(full_endpoint, secret_key, payload, data) {
   var date = new Date(payload.event_timestamp);
  
-  payload.date = date.getFullYear() + '/' + ("0" + (date.getMonth() + 1)).slice(-2) + '/' + ("0" + date.getDate()).slice(-2);
+  payload.date = date.getFullYear() + '-' + ("0" + (date.getMonth() + 1)).slice(-2) + '-' + ("0" + date.getDate()).slice(-2);
   payload.user_agent = navigator.userAgent;
   payload.browser = detectBrowser();
   payload.device = detectDevice();

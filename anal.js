600c7bad9160f05f6037bac110b2a42676f084ea
Tommaso Moretti <tommasomoretti88@gmail.com>
2023-12-01 14:27:19 +0100

    Update anal.js

--- anal.js
+++ anal.js
@@ -5,9 +5,11 @@ function sendData(full_endpoint, secret_key, payload, data) {
  
   payload.date = date.getFullYear() + '-' + ("0" + (date.getMonth() + 1)).slice(-2) + '-' + ("0" + date.getDate()).slice(-2);
   payload.user_agent = navigator.userAgent;
-  payload.browser = detectBrowser();
-  payload.device = detectDevice();
-  payload.platform = detectOS();
+  payload.device_brand = getDeviceBrand();
+  payload.device_type = getDeviceType();
+  payload.os = getOS();
+  payload.os_version = getOSVersion();
+  payload.browser = getBrowser();
   
   if(data.enable_logs){console.log('👉 Request payload: ', payload);}
   if(data.enable_logs){console.log('🟢 Analytics consent granted. Sending request...');}
@@ -38,6 +40,120 @@ function sendData(full_endpoint, secret_key, payload, data) {
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 
 
+function getDeviceBrand() {
+    const userAgent = navigator.userAgent;
+
+    let deviceModel = "Modello sconosciuto";
+
+    // iPhone
+    if (userAgent.indexOf("iPhone") !== -1) {
+        deviceModel = userAgent.match(/iPhone\s*([\w\s]+)/);
+        deviceModel = deviceModel ? "iPhone " + deviceModel[1].trim() : deviceModel;
+    }
+
+    // iPad
+    if (userAgent.indexOf("iPad") !== -1) {
+        deviceModel = userAgent.match(/iPad\s*([\w\s]+)/);
+        deviceModel = deviceModel ? "iPad " + deviceModel[1].trim() : deviceModel;
+    }
+
+    // Android
+    if (userAgent.indexOf("Android") !== -1) {
+        deviceModel = userAgent.match(/Android\s*([\w\s]+)/);
+        deviceModel = deviceModel ? "Android " + deviceModel[1].trim() : deviceModel;
+    }
+
+    // Altri dispositivi (es. Windows Phone, BlackBerry)
+    if (userAgent.indexOf("Windows Phone") !== -1 || userAgent.indexOf("BlackBerry") !== -1) {
+        deviceModel = userAgent.match(/[^\/]+\/([^;\)]+)/);
+        deviceModel = deviceModel ? deviceModel[1].trim() : deviceModel;
+    }
+
+    return deviceModel;
+}
+
+
+function getDeviceType() {
+  const isMobile = /iPhone|iPad|iPod|Android|IEMobile|BlackBerry|Opera Mini/i.test(navigator.userAgent);
+  const isTablet = /iPad|Android|Tablet/i.test(navigator.userAgent);
+
+  if (isTablet) {
+    return "Tablet";
+  } else if (isMobile) {
+    return "Mobile";
+  } else {
+    return "Desktop";
+  }
+}
+
+
+function getOS() {
+    const userAgent = navigator.userAgent;
+
+    if (userAgent.indexOf("Windows NT 10.0") !== -1) {
+        return "Windows 10";
+    } else if (userAgent.indexOf("Windows NT 6.3") !== -1) {
+        return "Windows 8.1";
+    } else if (userAgent.indexOf("Windows NT 6.2") !== -1) {
+        return "Windows 8";
+    } else if (userAgent.indexOf("Windows NT 6.1") !== -1) {
+        return "Windows 7";
+    } else if (userAgent.indexOf("Windows NT 6.0") !== -1) {
+        return "Windows Vista";
+    } else if (userAgent.indexOf("Windows NT 5.1") !== -1) {
+        return "Windows XP";
+    } else if (userAgent.indexOf("Mac OS X") !== -1) {
+        return "Mac OS X";
+    } else if (userAgent.indexOf("Linux") !== -1) {
+        return "Linux";
+    } else if (userAgent.indexOf("Android") !== -1) {
+        return "Android";
+    } else if (userAgent.indexOf("iOS") !== -1) {
+        return "iOS";
+    } else {
+        return "Sistema operativo sconosciuto";
+    }
+}
+
+
+function getOSVersion() {
+    const userAgent = navigator.userAgent;
+
+    let osVersion = "Versione sconosciuta";
+
+    // Windows
+    if (userAgent.indexOf("Windows NT") !== -1) {
+        osVersion = userAgent.match(/Windows NT (\d+\.\d+)/);
+        osVersion = osVersion ? "Windows " + osVersion[1] : osVersion;
+    }
+
+    // Mac OS X
+    if (userAgent.indexOf("Mac OS X") !== -1) {
+        osVersion = userAgent.match(/Mac OS X (\d+[._]\d+[._]?\d*)/);
+        osVersion = osVersion ? "macOS " + osVersion[1].replace(/_/g, ".") : osVersion;
+    }
+
+    // Linux
+    if (userAgent.indexOf("Linux") !== -1) {
+        osVersion = "Linux";
+    }
+
+    // Android
+    if (userAgent.indexOf("Android") !== -1) {
+        osVersion = userAgent.match(/Android (\d+[._]\d+[._]?\d*)/);
+        osVersion = osVersion ? "Android " + osVersion[1] : osVersion;
+    }
+
+    // iOS
+    if (userAgent.indexOf("iPhone") !== -1 || userAgent.indexOf("iPad") !== -1) {
+        osVersion = userAgent.match(/OS (\d+[._]\d+[._]?\d*)/);
+        osVersion = osVersion ? "iOS " + osVersion[1].replace(/_/g, ".") : osVersion;
+    }
+
+    return osVersion;
+}
+
+
 function getBrowser() {
   const userAgent = navigator.userAgent;
 
@@ -56,21 +172,4 @@ function getBrowser() {
   } else {
     return "Browser sconosciuto";
   }
-}
-
-
-function detectDevice(){
-  let userAgent = navigator.userAgent;
-  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(userAgent)) {
-    return "Tablet";
-  }
-  else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(userAgent)) {
-    return "Mobile";
-  }
-  return "Desktop";
-}
-
-
-function detectOS(){
-  return null
 }

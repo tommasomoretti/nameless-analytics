cdc0dde30717a11a99fdba96092df9e2252f0116
Tommaso Moretti <tommasomoretti88@gmail.com>
2023-12-02 09:52:06 +0100

    Update anal.js

--- anal.js
+++ anal.js
@@ -3,12 +3,15 @@
 function sendData(full_endpoint, secret_key, payload, data) {
   payload.date = formatDatetime(payload.timestamp).split("T")[0];
   payload.date_time = formatDatetime(payload.timestamp);
-  payload.user_agent = navigator.userAgent;
-  payload.device_brand = getDeviceBrand();
-  payload.device_type = getDeviceType();
-  payload.os = getOS();
-  payload.os_version = getOSVersion();
-  payload.browser = getBrowser();
+  
+  payload.user_agent = parseUa().ua;
+  payload.browser_name = parseUa().browser.name;
+  payload.browser_version = parseUa().browser.version;
+  payload.device_vendor = parseUa().device.vendor;
+  payload.device_model = parseUa().device.model;
+  payload.device_type = parseUa().device.type;
+  payload.os_name = parseUa().os.name;
+  payload.os_version = parseUa().os.version;
   
   if(data.enable_logs){console.log('👉 Request payload: ', payload);}
   if(data.enable_logs){console.log('🟢 Analytics consent granted. Sending request...');}
@@ -54,137 +57,8 @@ function formatDatetime(timestamp) {
   return formattedDate;
 }
 
-
-function getDeviceBrand() {
-    const userAgent = navigator.userAgent;
-
-    let deviceModel = "Modello sconosciuto";
-
-    // iPhone
-    if (userAgent.indexOf("iPhone") !== -1) {
-        deviceModel = userAgent.match(/iPhone\s*([\w\s]+)/);
-        deviceModel = deviceModel ? "iPhone " + deviceModel[1].trim() : deviceModel;
-    }
-
-    // iPad
-    if (userAgent.indexOf("iPad") !== -1) {
-        deviceModel = userAgent.match(/iPad\s*([\w\s]+)/);
-        deviceModel = deviceModel ? "iPad " + deviceModel[1].trim() : deviceModel;
-    }
-
-    // Android
-    if (userAgent.indexOf("Android") !== -1) {
-        deviceModel = userAgent.match(/Android\s*([\w\s]+)/);
-        deviceModel = deviceModel ? "Android " + deviceModel[1].trim() : deviceModel;
-    }
-
-    // Altri dispositivi (es. Windows Phone, BlackBerry)
-    if (userAgent.indexOf("Windows Phone") !== -1 || userAgent.indexOf("BlackBerry") !== -1) {
-        deviceModel = userAgent.match(/[^\/]+\/([^;\)]+)/);
-        deviceModel = deviceModel ? deviceModel[1].trim() : deviceModel;
-    }
-
-    return deviceModel;
-}
-
-
-function getDeviceType() {
-  const isMobile = /iPhone|iPad|iPod|Android|IEMobile|BlackBerry|Opera Mini/i.test(navigator.userAgent);
-  const isTablet = /iPad|Android|Tablet/i.test(navigator.userAgent);
-
-  if (isTablet) {
-    return "Tablet";
-  } else if (isMobile) {
-    return "Mobile";
-  } else {
-    return "Desktop";
-  }
-}
-
-
-function getOS() {
-    const userAgent = navigator.userAgent;
-
-    if (userAgent.indexOf("Windows NT 10.0") !== -1) {
-        return "Windows 10";
-    } else if (userAgent.indexOf("Windows NT 6.3") !== -1) {
-        return "Windows 8.1";
-    } else if (userAgent.indexOf("Windows NT 6.2") !== -1) {
-        return "Windows 8";
-    } else if (userAgent.indexOf("Windows NT 6.1") !== -1) {
-        return "Windows 7";
-    } else if (userAgent.indexOf("Windows NT 6.0") !== -1) {
-        return "Windows Vista";
-    } else if (userAgent.indexOf("Windows NT 5.1") !== -1) {
-        return "Windows XP";
-    } else if (userAgent.indexOf("Mac OS X") !== -1) {
-        return "Mac OS X";
-    } else if (userAgent.indexOf("Linux") !== -1) {
-        return "Linux";
-    } else if (userAgent.indexOf("Android") !== -1) {
-        return "Android";
-    } else if (userAgent.indexOf("iOS") !== -1) {
-        return "iOS";
-    } else {
-        return "Sistema operativo sconosciuto";
-    }
-}
-
-
-function getOSVersion() {
-    const userAgent = navigator.userAgent;
-
-    let osVersion = "Versione sconosciuta";
-
-    // Windows
-    if (userAgent.indexOf("Windows NT") !== -1) {
-        osVersion = userAgent.match(/Windows NT (\d+\.\d+)/);
-        osVersion = osVersion ? "Windows " + osVersion[1] : osVersion;
-    }
-
-    // Mac OS X
-    if (userAgent.indexOf("Mac OS X") !== -1) {
-        osVersion = userAgent.match(/Mac OS X (\d+[._]\d+[._]?\d*)/);
-        osVersion = osVersion ? "macOS " + osVersion[1].replace(/_/g, ".") : osVersion;
-    }
-
-    // Linux
-    if (userAgent.indexOf("Linux") !== -1) {
-        osVersion = "Linux";
-    }
-
-    // Android
-    if (userAgent.indexOf("Android") !== -1) {
-        osVersion = userAgent.match(/Android (\d+[._]\d+[._]?\d*)/);
-        osVersion = osVersion ? "Android " + osVersion[1] : osVersion;
-    }
-
-    // iOS
-    if (userAgent.indexOf("iPhone") !== -1 || userAgent.indexOf("iPad") !== -1) {
-        osVersion = userAgent.match(/OS (\d+[._]\d+[._]?\d*)/);
-        osVersion = osVersion ? "iOS " + osVersion[1].replace(/_/g, ".") : osVersion;
-    }
-
-    return osVersion;
-}
-
-
-function getBrowser() {
-  const userAgent = navigator.userAgent;
-
-  if (userAgent.indexOf("Firefox") !== -1) {
-    return "Mozilla Firefox";
-  } else if (userAgent.indexOf("Chrome") !== -1) {
-    return "Google Chrome";
-  } else if (userAgent.indexOf("Safari") !== -1) {
-    return "Apple Safari";
-  } else if (userAgent.indexOf("Edge") !== -1) {
-    return "Microsoft Edge";
-  } else if (userAgent.indexOf("Opera") !== -1 || userAgent.indexOf("OPR") !== -1) {
-    return "Opera";
-  } else if (userAgent.indexOf("Trident") !== -1) {
-    return "Internet Explorer";
-  } else {
-    return "Browser sconosciuto";
-  }
+function parseUa(){
+  var uap = new UAParser();
+  var uap_res = uap.getResult();
+  return uap_res
 }

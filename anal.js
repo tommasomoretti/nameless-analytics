0c51eefe35c5094d739bb80623e8458ba0df960c
Tommaso Moretti <tommasomoretti88@gmail.com>
2024-04-13 12:06:17 +0200

    Update anal.js

--- anal.js
+++ anal.js
@@ -17,7 +17,7 @@ function sendData(full_endpoint, secret_key, payload, data) {
   payload.event_data.screen_size = window.screen.width + "x" + window.screen.height;
   payload.event_data.wiewport_size = window.innerWidth + "x" + window.innerHeight;
 
-  if(data.config_variable.enable_logs){log('  Event data);}
+  if(data.config_variable.enable_logs){log('  Event data');}
   if(data.config_variable.enable_logs){log('    👉 Event name: ' + payload.event_name);}
   if(data.config_variable.enable_logs){console.log('    👉 Request payload: ', payload);}
 
@@ -33,14 +33,14 @@ function sendData(full_endpoint, secret_key, payload, data) {
   })
   .then((response) => response.json())
   .then((response_json) => {
-    if(data.config_variable.enable_logs){console.log('Request response';}
+    if(data.config_variable.enable_logs){console.log('Request response');}
     if(data.config_variable.enable_logs){console.log(response_json.response)}
     if (response_json.status_code === 200)
       return data.gtmOnSuccess()
     else return data.gtmOnFailure()
   })
   .catch((error) => {
-    if(data.config_variable.enable_logs){console.log('Request response';}
+    if(data.config_variable.enable_logs){console.log('Request response');}
     if(data.config_variable.enable_logs){console.log(error)}
     return data.gtmOnFailure()
   })

dd2514bd163c3bad3d914f8e27b1ed096c64e171
Tommaso Moretti <tommasomoretti88@gmail.com>
2024-04-16 09:40:01 +0200

    Update anal.js

--- anal.js
+++ anal.js
@@ -1,46 +1,45 @@
 // Send hits
 
 function sendData(full_endpoint, payload, data) {
-  console.log('send data ok')
-  // payload.date = formatDatetime(payload.timestamp).split("T")[0]
-  // payload.date_time = formatDatetime(payload.timestamp)
-// 
-  // ua_info = parseUa()
-  // payload.session_data.user_agent = ua_info.ua
-  // payload.session_data.browser_name = ua_info.browser.name
-  // payload.session_data.browser_version = ua_info.browser.version
-  // payload.session_data.browser_language = navigator.language || navigator.userLanguage 
-  // payload.session_data.device_type = ua_info.device.type || "desktop"
-  // payload.session_data.device_vendor = ua_info.device.vendor
-  // payload.session_data.device_model = ua_info.device.model
-  // payload.session_data.os_name = ua_info.os.name
-  // payload.session_data.os_version = ua_info.os.version
-  // payload.event_data.screen_size = window.screen.width + "x" + window.screen.height
-  // payload.event_data.wiewport_size = window.innerWidth + "x" + window.innerHeight
-  // 
-  // if(data.config_variable.enable_logs){console.log('  Event data')}
-  // if(data.config_variable.enable_logs){console.log('    👉 Event name: ' + payload.event_name)}
-  // if(data.config_variable.enable_logs){console.log('    👉 Request payload: ', payload)}
-// 
-  // fetch(full_endpoint, {
-  //   method: 'POST',
-  //   credentials: 'include',
-  //   mode: 'cors',
-  //   body: JSON.stringify(payload)
-  // })
-  // .then((response) => response.json())
-  // .then((response_json) => {
-  //   if(data.config_variable.enable_logs){console.log('  Request response')}
-  //   if(data.config_variable.enable_logs){console.log('    ' + response_json.response)}
-  //   if (response_json.status_code === 200)
-  //     return data.gtmOnSuccess()
-  //   else return data.gtmOnFailure()
-  // })
-  // .catch((error) => {
-  //   if(data.config_variable.enable_logs){console.log('Request response')}
-  //   if(data.config_variable.enable_logs){console.log(error)}
-  //   return data.gtmOnFailure()
-  // })
+  payload.date = formatDatetime(payload.timestamp).split("T")[0]
+  payload.date_time = formatDatetime(payload.timestamp)
+  
+  ua_info = parseUa()
+  payload.session_data.user_agent = ua_info.ua
+  payload.session_data.browser_name = ua_info.browser.name
+  payload.session_data.browser_version = ua_info.browser.version
+  payload.session_data.browser_language = navigator.language || navigator.userLanguage 
+  payload.session_data.device_type = ua_info.device.type || "desktop"
+  payload.session_data.device_vendor = ua_info.device.vendor
+  payload.session_data.device_model = ua_info.device.model
+  payload.session_data.os_name = ua_info.os.name
+  payload.session_data.os_version = ua_info.os.version
+  payload.event_data.screen_size = window.screen.width + "x" + window.screen.height
+  payload.event_data.wiewport_size = window.innerWidth + "x" + window.innerHeight
+  
+  if(data.config_variable.enable_logs){console.log('  Event data')}
+  if(data.config_variable.enable_logs){console.log('    👉 Event name: ' + payload.event_name)}
+  if(data.config_variable.enable_logs){console.log('    👉 Request payload: ', payload)}
+  
+  fetch(full_endpoint, {
+    method: 'POST',
+    credentials: 'include',
+    mode: 'cors',
+    body: JSON.stringify(payload)
+  })
+  .then((response) => response.json())
+  .then((response_json) => {
+    if(data.config_variable.enable_logs){console.log('  Request response')}
+    if(data.config_variable.enable_logs){console.log('    ' + response_json.response)}
+    if (response_json.status_code === 200)
+      return data.gtmOnSuccess()
+    else return data.gtmOnFailure()
+  })
+  .catch((error) => {
+    if(data.config_variable.enable_logs){console.log('Request response')}
+    if(data.config_variable.enable_logs){console.log(error)}
+    return data.gtmOnFailure()
+  })
 }
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------

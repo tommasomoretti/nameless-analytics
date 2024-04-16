3f17c4540175b8945907fe47facd53f935ab9829
Tommaso Moretti <tommasomoretti88@gmail.com>
2024-04-16 17:28:04 +0200

    Update anal.js

--- anal.js
+++ anal.js
@@ -110,7 +110,9 @@ function getCampaign(source, campaign) {
   const search_engine = new RegExp('.*google.*|.*bing.*|.*yahoo.*|.*baidu.*|.*yandex.*|.*duckduckgo.*|.*ask.*|.*aol.*|.*ecosia.*')
   const social_network = new RegExp('.*facebook.*|.*messenger.*|.*instagram.*|.* tiktok.*|.*t\.com\.*|.*twitter.*|.*linkedin.*|.*pinterest.*|.*youtube.*|.*whatsapp.*|.*wechat.*')
 
-  if (source == null) {
+  if (source == 'tagassistant.google.com'){
+    medium = 'debug'
+  } else if (source == null) {
     medium = null
   } else if (source == 'direct') {
     medium = 'none'

685086217a70809fdb01573c8c9718019497fcf7
Tommaso Moretti <tommasomoretti88@gmail.com>
2024-05-06 15:43:38 +0200

    Update nameless_analytics.js

--- nameless_analytics.js
+++ nameless_analytics.js
@@ -101,7 +101,7 @@ function getChannelGroup(referrer_hostname, source, campaign) {
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 
-window.addEventListener('beforeunload', function (event) {  
+window.addEventListener('unload', function (event) {  
   window.dataLayer = window.dataLayer || [];
   window.dataLayer.push({
     event: 'page_closed'
